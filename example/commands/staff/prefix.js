4b470be1eb96c949ddd0f5e4ced62aea034d2cb4
defowler2005 <defowler2005@gmail.com>
2023-12-05 13:55:56 +1030

    jk

---
+++ example/commands/staff/prefix.js
@@ -0,0 +1,40 @@
+const fs = require('fs');
+const commandBuild = require('../../../library/build/classes/commandBuilder.js')
+
+const configFilePath = './library/build/config.json';
+
+commandBuild.create(
+    {
+        name: 'prefix',
+        description: 'Set the command prefix',
+        is_staff: true,
+    }, (message, args) => {
+        if (!args[0]) {
+            return message.reply('Please enter arguments.');
+        }
+        if (args[0].length > 6) {
+            return message.reply('Invalid prefix. Prefix must not exceed 6 characters.');
+        }
+
+        const newPrefix = args[0];
+
+        fs.readFile(configFilePath, 'utf8', (error, data) => {
+            if (error) {
+                console.error('Error reading file:', error);
+                return message.reply('An error occurred while reading the configuration file.');
+            }
+
+            const jsonData = JSON.parse(data);
+            jsonData.chatCmdPrefix = newPrefix;
+
+            fs.writeFile(configFilePath, JSON.stringify(jsonData, null, 2), 'utf8', (err) => {
+                if (err) {
+                    console.error('Error writing to file:', err);
+                    return message.reply('An error occurred while updating the prefix.');
+                }
+
+                message.reply(`Chat command prefix updated to: ${newPrefix}`);
+            });
+        });
+    }
+);
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const { Client, GatewayIntentBits, Events, REST, Routes } = require('discord.js');
 const commandBuild = require('./library/build/classes/commandBuilder.js');
 const { token, guildId, clientId } = require('./library/build/config.json');
+require('./example/commands/staff/prefix.js')
 const fs = require('fs');
 
 const client = new Client(
