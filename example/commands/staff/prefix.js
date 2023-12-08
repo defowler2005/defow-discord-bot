f265081f0b5303a9170c40172fa15d6c34722764
defowler2005 <defowler2005@gmail.com>
2023-12-08 19:47:26 +1030

    d

--- example/commands/staff/prefix.js
+++ example/commands/staff/prefix.js
@@ -7,29 +7,21 @@ commandBuild.create(
         description: 'Set the command prefix',
         is_staff: true,
     }, (message, args) => {
-        if (!args[0]) {
-            return message.reply('Please enter arguments.');
-        }
-        if (args[0].length > 6) {
-            return message.reply('Invalid prefix. Prefix must not exceed 6 characters.');
-        }
-
+        if (!args[0]) return message.reply('Please enter arguments.');
+        if (args[0].length > 6) return message.reply('Invalid prefix. Prefix must not exceed 6 characters.');
         fs.readFile('./library/build/config.json', 'utf8', (error, data) => {
             if (error) {
                 console.error('Error reading file:', error);
                 return message.reply('An error occurred while reading the configuration file.');
             }
-
             const jsonData = JSON.parse(data);
-            jsonData.chatCmdPrefix = args[0];
-
+            jsonData.chatCmdPrefix = `${args[0]}`;
             fs.writeFile('./library/build/config.json', JSON.stringify(jsonData, null, 2), 'utf8', (err) => {
                 if (err) {
                     console.error('Error writing to file:', err);
                     return message.reply('An error occurred while updating the prefix.');
-                };
-                message.reply(`# Chat command prefix updated to: `+ '`' + args[0] + '`' );
-            });
-        });
+                }; message.reply(`# Chat command prefix updated to: ` + '`' + args[0] + '`');
+            })
+        })
     }
 );
