80f2a18fff7567443ce471b17071dedb241033f4
defowler2005 <defowler2005@gmail.com>
2024-01-11 01:29:20 +1030

    v

--- example/commands/user/userInfo.cjs
+++ example/commands/user/userInfo.cjs
@@ -10,28 +10,27 @@ commandBuild.create(
     }, (message, args) => {
         const targetUser = message.mentions.users.first() || message.guild.members.cache.get(args[0]) || message.author;
 
-        const embed1 = new embedBuild();
-        const embedInfo = {
-            setColor: '#FF6200',
-            title: 'User Information',
-            authorName: targetUser.username,
-            authorIconURL: targetUser.avatarURL(),
-            description: `Information about ${targetUser}`,
-            fields: [
-                { name: 'ID', value: targetUser.id },
-                { name: 'Username', value: targetUser.username },
-                { name: 'Tag', value: targetUser.tag },
-                { name: 'Bot?', value: targetUser.bot ? 'Yes' : 'No', inline: true },
-                { name: 'Created At', value: targetUser.createdAt.toLocaleString(), inline: true },
-            ],
-            thumbnailURL: targetUser.avatarURL(),
-            timestamp: new Date().toISOString(),
-            footerText: 'User Info by defowler',
-            footerIconURL: 'https://i.imgur.com/AfFp7pu.png'
-        };
-
-        embed1.create(embedInfo, (embed) => {
-            message.channel.send({ embeds: [embed] });
-        })
+        new embedBuild().create(
+            {
+                setColor: '#FF6200',
+                title: 'User Information',
+                authorName: targetUser.username,
+                authorIconURL: targetUser.avatarURL(),
+                description: `Information about ${targetUser}`,
+                fields: [
+                    { name: 'ID', value: targetUser.id },
+                    { name: 'Username', value: targetUser.username },
+                    { name: 'Tag', value: targetUser.tag },
+                    { name: 'Bot?', value: targetUser.bot ? 'Yes' : 'No', inline: true },
+                    { name: 'Created At', value: targetUser.createdAt.toLocaleString(), inline: true },
+                ],
+                thumbnailURL: targetUser.avatarURL(),
+                timestamp: Date.now(),
+                footerText: 'User Info by defowler',
+                footerIconURL: 'https://i.imgur.com/AfFp7pu.png'
+            }, (embed) => {
+                message.channel.send({ embeds: [embed] });
+            }
+        )
     }
 );
